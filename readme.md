96f4da2473f81d94d5ab3974b293a14882de86ff
Mateusz Skibicki <skibickiengland@gmail.com>
2019-10-21 10:09:45 +0100

    readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+# React (16.8+ Hooks!) / React-Native / Redux
+
+### Expo CLI (sdk 35) / Redux / React-Navigation (v4) / Typescript / Jest
+
+Setup is ready with navigation auth flow (createStackNavigator, createMaterialBottomTabNavigator, createAppContainer, createSwitchNavigator) and you've got:
+
+- AuthLoading (initialRouteName -> here it will check for token and go to Auth or Home)
+- AuthLogout (remove token and go to Auth)
+- Auth (Login/Logout screens)
+- Home (main part with app)
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, Middleware, Store } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const middleware = [thunk];
+const middleware: [Middleware<any>] = [thunk];
 
-export default function initializeStore(initialState) {
+export default function initializeStore(initialState): Store {
   return createStore(
     rootReducer,
     initialState,
